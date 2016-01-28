14e36b7c9457e01448bbdd0d7acc9a262f8a8fda
Seokju Hong <tjkj555@gmail.com>
2016-01-29 00:35:45 +0900

    Revised travis files and test codes

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
+/* jshint expr: true */
 var should = require('should');
 
 describe('example', function() {
   describe('example suite', function() {
     it('should be like this', function() {
       true.should.be.true;
-    })
-  })
+    });
+  });
 });
