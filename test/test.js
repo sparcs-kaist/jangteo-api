71d99afd7782cdeed20910362600ee4b8a5127a5
Seokju Hong <tjkj555@gmail.com>
2016-01-29 00:06:08 +0900

    Integrated mocha.js test framework to project

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var should = require('should');
+
+describe('example', function() {
+  describe('example suite', function() {
+    it('should be like this', function() {
+      true.should.be.true;
+    })
+  })
+});
