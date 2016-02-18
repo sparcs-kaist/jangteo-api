fd4917389f773072fd4d8aa2c3c12e42dd5c17dc
Sam Jo <a1sams1a@gmail.com>
2016-02-18 19:49:54 +0900

    add created / lastUpdated time

--- common/models/person.js
+++ common/models/person.js
@@ -43,32 +43,41 @@ module.exports = function(Person) {
         defaultError.statusCode = 401;
         defaultError.code = 'LOGIN_FAILED';
 
+        var now = new Date();
+        var nowStr = now.toISOString().slice(0, 10);
+
         if (err) {
           debug('An error is reported from Person.findOne: %j', err);
           fn(defaultError);
         } else if (person) {
-          if (person.createAccessToken.length === 2) {
-            person.createAccessToken(undefined, fn);
-          } else {
-            person.createAccessToken(undefined, tokenid, fn);
-          }
+          person.updateAttribute('lastUpdated', nowStr, function(err, person) {
+            if (err) {
+              fn(defaultError);
+            } else if (person.createAccessToken.length === 2) {
+              person.createAccessToken(undefined, fn);
+            } else {
+              person.createAccessToken(undefined, tokenid, fn);
+            }
+          });
         } else {
           debug('No matching record is found for person %s', query.username);
           
+          var now = new Date();
           var newInfo = {
             sid: info.sid,
-            nickname: 'need to set',
-            contact: 'need to set',
+            nickname: 'NEED-TO-SET',
+            nicknameUpdateTime: '1999-12-31',
+            contact: 'NEED-TO-SET',
             credit: 0,
-            status: 'new',
+            status: 'NEW',
             email: info.email,
             password: 'DuMMyPa55w0rD!',
+            created: nowStr
           };
 
           Person.create(newInfo, function(err, person) {
             if (err) {
               fn(err);
-              //fn(defaultError);
             } else if (person.createAccessToken.length === 2) {
               person.createAccessToken(undefined, fn);
             } else {
